c45342c15f740e418be995a633eb289868bf0696
Clarkxielf <71970105+Clarkxielf@users.noreply.github.com>
2024-03-26 20:06:23 +0800

    Add files via upload

---
+++ HKD_2_Cifar100/hkd_train.py
@@ -0,0 +1,126 @@
+import torch
+import torch.nn as nn
+
+from model import resnet56,resnet32,Sum_weights
+
+import os
+from torch.utils.tensorboard import SummaryWriter
+
+import numpy as np
+import scipy.io as sio
+from losses import dkd_loss, FM_LOSS
+from dataset import get_cifar100_dataloaders_sample
+
+def train_once_data(num):
+    torch.manual_seed(1)
+    Sampling_Interval = 1
+    split_ratio = 0.8
+    BATCH_SIZE = 64
+    EPOCH = 240
+    LR = 0.05
+
+
+    train_loader, test_loader, num_data = get_cifar100_dataloaders_sample(64, 64, 0, 4096, mode="exact")
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    teacher = resnet56()
+    student = resnet32()
+    weght = Sum_weights().to(device)
+
+    if torch.cuda.device_count() >= 1:
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
+        teacher = nn.DataParallel(teacher)
+        student = nn.DataParallel(student)
+
+    load_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'HKD1_6.pkl')
+    teacher.load_state_dict(torch.load(load_path, map_location=device))
+    teacher.to(device)
+    student.to(device)
+
+    optimizer = torch.optim.SGD(student.parameters(), lr=LR, momentum=0.9, weight_decay=5e-4)
+    loss_task = nn.CrossEntropyLoss()
+    loss_fm = FM_LOSS().cuda()
+
+    best_accuracy = 0.
+    save_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'HKD2_{}.pkl'.format(num))
+
+    writer = SummaryWriter(comment='HKD_4')
+    Loss = []
+    Acc_rate =[]
+
+    for epoch in range(EPOCH):
+
+        if epoch == 150 or epoch == 180 or epoch == 210 :
+            LR = LR*0.1
+            optimizer = torch.optim.SGD(student.parameters(), lr=LR, momentum=0.9, weight_decay=5e-4)
+
+
+        student.train()
+        loss_sum = 0.
+        for step,  b_y in enumerate(train_loader):
+
+
+            f_t, logits_t = teacher(b_y[0].to(device))
+            f_s, logits_s = student(b_y[0].to(device))
+            [a, b, c] = weght(f_s)
+            loss1 = loss_fm(f_s, f_t)
+            loss2 = dkd_loss(logits_s, logits_t, b_y[1].to(device), alpha = 1, beta = 2, temperature = 4)
+            loss3 = loss_task(logits_s, b_y[1].to(device))
+            loss = a * loss1 + b * loss2 + c * loss3
+            loss_sum += loss
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        Loss.append(loss_sum.data.cpu().numpy())
+
+        student.eval()
+        correct = 0
+        with torch.no_grad():
+            for _,  b_y in enumerate(test_loader):
+
+                _, test_output = student(b_y[0].to(device))
+                pred_y = torch.max(test_output, dim=1)[1]
+
+                correct += (pred_y == b_y[1].to(device)).sum().item()
+
+            accuracy = correct / len(test_loader.dataset)
+            Acc_rate.append(accuracy)
+            print('Time: ', num+1, '| Epoch: ', epoch,'|train_loss: %.4f' % loss_sum.item(),
+                  '|test_accuracy: %.4f' % accuracy, '({}/{})'.format(correct, len(test_loader.dataset)))
+
+            if accuracy>best_accuracy:
+                best_accuracy = accuracy
+                torch.save(student.state_dict(), save_path)
+
+
+                for _,  b_y in enumerate(test_loader):
+                    # x=FFT(b_x)
+                    _, test_output = student(b_y[0].to(device))
+                    pred_y = torch.max(test_output, dim=1)[1]
+
+
+
+        writer.add_scalar('train_loss', loss_sum, epoch)
+        writer.add_scalar('test_accuracy', accuracy, epoch)
+
+    Loss_data=np.stack(Loss,axis=0)
+    Acc_data=np.stack(Acc_rate,axis=0)
+    # sio.savemat('loss_hkd{}.mat'.format(num),{'loss':Loss_data})
+    sio.savemat('accuracy_hkd{}.mat'.format(num),{'accuracy':Acc_data})
+    writer.close()
+    #打开Terminal,键入tensorboard --logdir=logs(logs是事件文件所保存路径)
+
+    os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+    return best_accuracy
+
+accuracy_rate = []
+for num in range(0,5):
+    best_acc = train_once_data(num)
+    print(best_acc)
+    accuracy_rate.append(best_acc)
+mean_accuracy = np.mean(accuracy_rate)
+std = np.std(accuracy_rate, ddof=1)
+print(mean_accuracy, '\n', std)
---
+++ HKD_2_Cifar100/losses1.py
@@ -0,0 +1,107 @@
+import torch
+import os
+import  torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
+from torch import einsum
+from einops import rearrange
+
+'''教师网络和学生网络特征转换一致'''
+class Embed(nn.Module):
+    def __init__(self, dim_in=64, dim_out=64):
+        super(Embed,self).__init__()
+        self.conv = nn.Conv2d(dim_in, dim_out, kernel_size=1, stride=1, bias=False)
+        self.norm = nn.BatchNorm2d(dim_out)
+
+    def forward(self,x):
+        x = self.conv(x)
+        x = self.norm(x)
+
+        return x
+
+'''学生网络语义特征与教师网络进行对齐的损失'''
+class FM_LOSS(nn.Module):
+    def __init__(self, heads = 8):
+        super(FM_LOSS, self).__init__()
+        self.embed = Embed()
+        self.heads = heads
+
+    def forward(self,f_s, f_t):
+        s, q, v = f_s, f_s,f_s # student feature, query, value
+        t, k = f_t, f_t # teacher feature, key
+        heads = self.heads  # multi head, heads*c = d
+        b, c, h, w = v.shape
+
+        q = rearrange(q, 'b (h d) x y -> b h (x y) d', h=heads)  # (b,heads,hw,d)
+        k = rearrange(k, 'b (h d) x y -> b h (x y) d', h=heads)  # (b,heads,hw,d)
+        v = rearrange(v, 'b (h d) x y -> b h (x y) d', h=heads)  # (b,heads,hw,d)
+
+        sim = einsum('b h i d, b h j d -> b h i j', q, k)  # (b,heads,hw,hw)
+        sim = sim.softmax(dim=-1)
+        out = einsum('b h i j, b h j d -> b h i d', sim, v)  # (b,heads,hw,d)
+        out = rearrange(out, 'b h n d -> b (h d) n')
+        out=torch.unsqueeze(out,-1)
+
+
+
+        
+        loss = nn.MSELoss()(out, t)
+
+
+
+        return  loss
+
+
+'''目标类与非目标类损失分别计算为tckd与nckd'''
+def dkd_loss(logits_student, logits_teacher, target, alpha, beta, temperature):
+    gt_mask = _get_gt_mask(logits_student, target)
+    other_mask = _get_other_mask(logits_student, target)
+    pred_student = F.softmax(logits_student / temperature, dim=1)
+    pred_teacher = F.softmax(logits_teacher / temperature, dim=1)
+    pred_student = cat_mask(pred_student, gt_mask, other_mask)
+    pred_teacher = cat_mask(pred_teacher, gt_mask, other_mask)
+    log_pred_student = torch.log(pred_student)
+    tckd_loss = (
+        F.kl_div(log_pred_student, pred_teacher, reduction= 'batchmean')
+        * (temperature**2)
+        / target.shape[0]
+    )
+    pred_teacher_part2 = F.softmax(
+        logits_teacher / temperature - 1000.0 * gt_mask, dim=1
+    )
+    log_pred_student_part2 = F.log_softmax(
+        logits_student / temperature - 1000.0 * gt_mask, dim=1
+    )
+    nckd_loss = (
+        F.kl_div(log_pred_student_part2, pred_teacher_part2, reduction= 'batchmean')
+        * (temperature**2)
+        / target.shape[0]
+    )
+    return alpha * tckd_loss + beta * nckd_loss
+
+
+def _get_gt_mask(logits, target):
+    target = target.reshape(-1)
+    mask = torch.zeros_like(logits).scatter_(1, target.unsqueeze(1), 1).bool()
+    return mask
+
+
+def _get_other_mask(logits, target):
+    target = target.reshape(-1)
+    mask = torch.ones_like(logits).scatter_(1, target.unsqueeze(1), 0).bool()
+    return mask
+
+def cat_mask(t, mask1, mask2):
+    t1 = (t * mask1).sum(dim=1, keepdims=True)
+    t2 = (t * mask2).sum(1, keepdims=True)
+    rt = torch.cat([t1, t2], dim=1)
+    return rt
+
+
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# fs = torch.randn(24, 512, 9)
+# ft = torch.randn(24, 2048, 9)
+# loss = FM_LOSS().cuda()
+# loss = loss(fs.to(device), ft.to(device))
+# print(loss)python
+
